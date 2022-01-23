aff1fefe81526f612f96b404be59047371a85dec
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-01-23 22:00:29 +0000

    Adds the NavBar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,44 +1,64 @@
-import { CloseIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, MenuIcon, Text } from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Logo() {
-  return <Text>Logo</Text>;
-}
-
-function AuthSection({ isLogged }) {
   return (
-    <>
-      {isLogged ? (
-        <AuthButton>Logoff</AuthButton>
-      ) : (
-        <Box>
-          <AuthButton mr="2">Login</AuthButton>
-          <AuthButton>Sign-Up</AuthButton>
-        </Box>
-      )}
-    </>
+    <Text as="h2" fontSize="xl" fontWeight="600">
+      MyBlog
+    </Text>
   );
 }
 
-function AuthButton(props) {
-  return <Button {...props}>{props.children}</Button>;
-}
-
-function MenuToggle({ toggle, isOpen }) {
+function MenuLink(props) {
   return (
-    <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon /> : <MenuIcon />}
-    </Box>
+    <Button as={Link} m="1" {...props}>
+      {props.children}
+    </Button>
   );
 }
 
 export default function Navbar({ user }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <Flex as="header" justify="space-between" padding="2">
-      <Logo />
-      <AuthSection isLogged={false} />
-      <MenuToggle />
+    <Flex
+      as="header"
+      justify="space-between"
+      padding="2"
+      direction={["column", "column", "row"]}
+      borderBottom={1}
+      borderStyle={"solid"}
+      borderColor="gray.200"
+    >
+      <Flex justify={{ base: "space-between" }} align="center">
+        <Logo />
+        <Button display={["block", "block", "none"]} onClick={toggleOpen}>
+          {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </Button>
+      </Flex>
+
+      
+        <Flex
+          as="nav"
+          display={{ base: isOpen ? "flex" : "none", md: "block" }}
+          direction={["column", "column"]}
+        >
+          <MenuLink to="/user/sign-up" variant={"link"} color="blue.600">
+            Sign-Up
+          </MenuLink>
+          <MenuLink
+            to="/user/login"
+            bg="blue.400"
+            color="white"
+            _hover="blue.300"
+          >
+            Login
+          </MenuLink>
+        </Flex>
     </Flex>
   );
 }
